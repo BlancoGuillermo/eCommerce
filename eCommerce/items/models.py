75ed7ce7f2f10c02e7979b9f10754d2e2cebfdf8
BlancoGuillermo <105031998+BlancoGuillermo@users.noreply.github.com>
2023-04-16 16:17:24 -0300

    Primera version

---
+++ eCommerce/items/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',) # ordena alfabéticamente
+        verbose_name_plural = "Categories"
+
+    # devuelve string con el nombre de items
+    def __str__(self):
+        return self.name
+    
+class Item(models.Model):
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True) # puede quedar en blanco
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE) # si se borra usuario también sus publicaciones lo harán
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # devuelve string con el nombre de items
+    def __str__(self):
+        return self.name
