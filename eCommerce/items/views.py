db02af7d523a086c5753bfab4035a0a9a74f76f0
Ivan Exequiel Carneiro <ivanexequielc@gmail.com>
2023-04-19 00:12:49 -0300

    ok

--- eCommerce/items/views.py
+++ eCommerce/items/views.py
@@ -7,7 +7,7 @@ def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     # Si un item pertenece a la misma categoria y no está vendido lo muestra en el template
     # related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
-    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:5]
 
 
     return render(request, 'item/detail.html', {
