212b1ccc55353b8cedc486508d905acf0b8d8020
Ivan Exequiel Carneiro <ivanexequielc@gmail.com>
2023-04-21 19:41:48 -0300

    pruebas

--- eCommerce/eCommerce/settings.py
+++ eCommerce/eCommerce/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'items',
+    'tailwind',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
--- eCommerce/items/views.py
+++ eCommerce/items/views.py
@@ -6,11 +6,10 @@ from .models import Item
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     # Si un item pertenece a la misma categoria y no está vendido lo muestra en el template
-    # related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:5]
 
 
-    return render(request, 'item/detail.html', {
+    return render(request, 'items/detail.html', {
         'item': item,
         'related_items': related_items,
         }
