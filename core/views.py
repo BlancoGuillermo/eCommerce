250079d166ecd69d2d88065e66651abc94dafa1c
Erica G <garceterica@outlook.com>
2023-05-23 19:47:18 -0300

    pull de cambios

--- core/views.py
+++ core/views.py
@@ -1,44 +1,4 @@
 from django.shortcuts import render
-<<<<<<< HEAD
-from django.contrib import messages
-from django.http import HttpResponseNotAllowed, HttpResponseRedirect
-from items.models import *
-from datetime import datetime
-from .forms import ContactoForm
-
-
-def index(request):
-    # filtra los items "NO_VENDIDOS", ordena por fecha de creación donde ultimo >>> primero, mostrando solo 4 elementos
-    items = Item.objects.filter(is_sold=False).order_by("-created_at")[:4]
-
-    categories = Category.objects.all()
-
-    return render( request, 'core/index.html', {
-        'categories': categories,
-        'items' : items,
-        }
-    )
-
-def contact(request):
-    if request.method == "POST":
-        contact_form = ContactoForm(request.POST)
-        mensaje = "Recibimos tu consulta. Gracias por escribirnos."
-        if contact_form.is_valid():
-            messages.success(request, mensaje)
-            contact_form = ContactoForm()
-        else:
-            messages.error(request, 'Los datos cargados tienen errores, revísalos')
-    elif request.method == "GET":
-        contact_form = ContactoForm()
-    else:
-        HttpResponseNotAllowed(f"Método {request.method} no soportado.")
-    
-    context = {
-        "contact_form": contact_form
-    }
-
-    return render(request, 'core/contact.html', context)
-=======
 from items.models import *
 # Create your views here.
 
@@ -54,4 +14,3 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
->>>>>>> 99f5923d28f2cdc0f465e8ecc331ae47c384e21a
--- items/views.py
+++ items/views.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-from django.shortcuts import render, redirect, get_object_or_404
-from .forms import *
-from .models import *
-
-=======
 from django.shortcuts import render, get_object_or_404
 from .models import Item
->>>>>>> 99f5923d28f2cdc0f465e8ecc331ae47c384e21a
 
 # Create your views here.
 
@@ -17,32 +10,8 @@ def detail(request, pk):
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:5]
 
 
-<<<<<<< HEAD
-    return render(request, 'items/detail.html', {
-=======
     return render(request, 'item/detail.html', {
->>>>>>> 99f5923d28f2cdc0f465e8ecc331ae47c384e21a
         'item': item,
         'related_items': related_items,
         }
     )
-<<<<<<< HEAD
-
-
-def publicar(request):
-    if request.method == 'POST':
-        form = ItemForm(request.POST, request.FILES)
-        if form.is_valid():
-            item = form.save(commit=False)
-            item.created_by = request.user
-            item.save()
-            images = request.FILES.getlist('images')
-            for image in images:
-                item_image = Image.objects.create(image=image)
-                item.images.add(item_image)
-            return redirect('item_detail', pk=item.pk)
-    else:
-        form = ItemForm()
-    return render(request, 'items/publicar.html', {'item_form': form})
-=======
->>>>>>> 99f5923d28f2cdc0f465e8ecc331ae47c384e21a
