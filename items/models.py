4cf40faf7d75248505b0802c4c394f0103b5bf37
Ivan Exequiel Carneiro <ivanexequielc@gmail.com>
2023-05-31 22:57:52 -0300

    mersh ok con ivan_2 >>> VBC ok

--- items/models.py
+++ items/models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.urls import reverse
 from django.db import models
 from django.contrib.auth.models import User
@@ -37,34 +36,3 @@ class Item(models.Model):
     
     class Meta:
         verbose_name_plural = 'Items'
-=======
-from django.contrib.auth.models import User
-from django.db import models
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=255)
-
-    class Meta:
-        ordering = ('name',) # ordena alfabéticamente
-        verbose_name_plural = "Categories"
-
-    # devuelve string con el nombre de items
-    def __str__(self):
-        return self.name
-
-
-class Item(models.Model):
-    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True) # puede quedar en blanco
-    price = models.FloatField()
-    image = models.ImageField(upload_to='item_images', blank=True, null=True) # blank y null debería ser False para impedir crear items sin imagen
-    is_sold = models.BooleanField(default=False)
-    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE) # si se borra usuario también sus publicaciones lo harán
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    # devuelve string con el nombre de items
-    def __str__(self):
-        return self.name
->>>>>>> main
